ee32af7bc12aadad7830f9c7309c3e5dfd69b137
Gershwen <76742941+Gershwen@users.noreply.github.com>
2022-01-06 12:36:35 +0200

    added google analytics

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -21,6 +21,16 @@ const Layout = (props) => {
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossOrigin="anonymous"
         />
+        {/* google analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TFLQGZ2EQE"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-TFLQGZ2EQE');
+        </script>
       </Head>
       <div style={layoutStyle}>
         <Header />
